345eb6587cbb1a02455ad6bc7c3c088251e2fae4
Sourav Malani <i190434@nu.edu.pk>
2023-09-15 03:25:06 +0500

    added email verification

---
+++ app/src/main/java/com/souravmalani/i190434/fragment_chat_inside_one.java
@@ -0,0 +1,18 @@
+package com.souravmalani.i190434;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class fragment_chat_inside_one extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_inside_one, container, false);
+    }
+}
